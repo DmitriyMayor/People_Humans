293428042b2c3db4d53f8cc309075de647811d93
dmitriymayorov <dmitriy85mayor@gmail.com>
2024-02-26 20:33:41 +0300

    Исправлены условия построения сообщения

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,17 +1,17 @@
 fun main(){
-    var likes = readln()
+    val likes = readln()
     //пока сделал переменную с вводом из консоли
 
-    if (likes.takeLast(1).equals("0")){
-        var people = "человек"
+    if (likes.toInt() == 0 || likes.takeLast(3).equals("000")){
+        val people = "человек"
         println("Понравилось $likes $people")
-    } else if (likes.takeLast(1).equals("0")){
-        var people = "человеку"
+    } else if (likes.takeLast(1).equals("1")&& !likes.takeLast(2).equals("11")){
+        val people = "человеку"
         println("Понравилось $likes $people")
-    }else {
-        var people = "человекам"
+    } else {
+        val people = "людям"
         println("Понравилось $likes $people")
     }
-
-
 }
+//println("Понравилось $likes $people")
+//likes.takeLast(1).equals("0")
